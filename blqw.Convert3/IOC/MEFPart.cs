c198de822c36575cb989896d4cea8dcb20313f7f
周子鉴 <fttl_398@126.com>
2016-03-16 09:49:26 +0800

    修复ioc插件bug

--- blqw.Convert3/IOC/MEFPart.cs
+++ blqw.Convert3/IOC/MEFPart.cs
@@ -188,7 +188,8 @@ namespace blqw.Convert3Component
                     continue;
                 }
                 var value = GetExportedValue(import);
-                f.SetValue(instance, value);
+                if (value != null)
+                    f.SetValue(instance, value);
             }
             var args = new object[1];
             foreach (var p in type.GetProperties(flags))
@@ -204,8 +205,11 @@ namespace blqw.Convert3Component
                     continue;
                 }
                 var value = GetExportedValue(import);
-                args[0] = value;
-                set.Invoke(instance, args);
+                if (value != null)
+                {
+                    args[0] = value;
+                    set.Invoke(instance, args);
+                }
             }
         }
 
