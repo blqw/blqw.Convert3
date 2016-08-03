a40c2a58952e140e787c6a61bddcac08b0865706
blqw <blqw@live.com>
2016-08-04 07:21:43 +0800

    1

--- UnitTest/异常测试.cs
+++ UnitTest/异常测试.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Data;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace blqw
 {
@@ -18,27 +19,34 @@ namespace blqw
         [TestMethod]
         public void 异常栈展示()
         {
-            var table1 = new DataTable("a");
-            table1.Columns.Add("id", typeof(int));
-            table1.Columns.Add("name", typeof(string));
-            table1.Rows.Add(1, "赵");
-            table1.Rows.Add(2, "钱");
-            table1.Rows.Add(3, "孙");
-            table1.Rows.Add(4, "李");
+            try
+            {
+                var table1 = new DataTable("a");
+                table1.Columns.Add("id", typeof(int));
+                table1.Columns.Add("name", typeof(string));
+                table1.Rows.Add(1, "赵");
+                table1.Rows.Add(2, "钱");
+                table1.Rows.Add(3, "孙");
+                table1.Rows.Add(4, "李");
 
-            var table2 = new DataTable("b");
-            table2.Columns.Add("id", typeof(int));
-            table2.Columns.Add("date", typeof(string));
-            table2.Rows.Add(1, "2016-04-20");
-            table2.Rows.Add(2, "2016-04-21");
-            table2.Rows.Add(3, "2016-04-22A");
-            table2.Rows.Add(4, "2016-04-23");
-            
-            var dataset = new DataSet();
-            dataset.Tables.Add(table1);
-            dataset.Tables.Add(table2);
+                var table2 = new DataTable("b");
+                table2.Columns.Add("id", typeof(int));
+                table2.Columns.Add("date", typeof(string));
+                table2.Rows.Add(1, "2016-04-20");
+                table2.Rows.Add(2, "2016-04-21");
+                table2.Rows.Add(3, "2016-04-22A");
+                table2.Rows.Add(4, "2016-04-23");
 
-            dataset.To<Dictionary<string, List<user>>>();
+                var dataset = new DataSet();
+                dataset.Tables.Add(table1);
+                dataset.Tables.Add(table2);
+
+                dataset.To<Dictionary<string, List<user>>>();
+            }
+            catch (AggregateException ex)
+            {
+                Trace.WriteLine(ex);
+            }
         }
 
         
